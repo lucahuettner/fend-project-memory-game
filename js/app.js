feddc53ef292edce6d140ea2068de418c9879956
Luca Huettner <luca.huettner@gmail.com>
2018-01-07 14:41:37 +0100

    added array for cards

--- js/app.js
+++ js/app.js
@@ -2,6 +2,24 @@
  * Create a list that holds all of your cards
  */
 
+let cards = [
+  'fa fa-diamond',
+  'fa fa-paper-plane-o',
+  'fa fa-anchor',
+  'fa fa-bolt',
+  'fa fa-cube',
+  'fa fa-leaf',
+  'fa fa-bomb',
+  'fa fa-bicycle',
+  'fa fa-diamond',
+  'fa fa-paper-plane-o',
+  'fa fa-anchor',
+  'fa fa-bolt',
+  'fa fa-cube',
+  'fa fa-leaf',
+  'fa fa-bomb',
+  'fa fa-bicycle'
+]
 
 /*
  * Display the cards on the page
